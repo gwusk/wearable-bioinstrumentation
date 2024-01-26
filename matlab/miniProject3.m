b90b5594daef125fb8d9a1ca98102e46e4b12e9e
Grace Wusk <gracew@vt.edu>
2024-01-26 15:40:52 -0500

    added comments

--- matlab/miniProject3.m
+++ matlab/miniProject3.m
@@ -2,8 +2,11 @@ clear
 clc
 close all
 
-% connect arduino
+%% Connect Arduino
+
 a = arduino();
+
+%% Collect and Save Data
 
 % define variables and call pressureSensor function
 sampleTime = 0;
@@ -12,16 +15,29 @@ livePlot = false;
 pauseTime = 0;
 [data] = pressureSensor(a,sampleTime,thresh,livePlot,pauseTime);
 
-% save pressureSensor output table to a csv in your data folder
-writetable(data,'C:\Users\username\git\wearable-bioinstrumentation\data\pressureSensorData.csv')
-
-% plot voltage over time
-figure
-plot(data.time,data.voltage)
-
 % calcuate data acqusition rate
 fs = 0;
 
 % calculate R2 using R1, Vin, and Vout
 r2 = 0;
 
+% save pressureSensor output table to a study array, table, or structure
+study = data;
+
+% save pressureSensor output table to a csv in your data folder
+writetable(data,'C:\Users\username\git\wearable-bioinstrumentation\data\pressureSensorData.csv')
+
+%% Figure 1. Calculate Resistance
+
+figure
+plot(data.time,data.voltage)
+
+%% Figure 2. Changing R1
+
+figure
+plot(data.time,data.voltage)
+
+%% Figure 3. Respiration
+
+figure
+plot(data.time,data.voltage)
